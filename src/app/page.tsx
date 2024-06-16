359147666f7e2f66619bf295f1ea2b0ad50ff92c
Victor <victor@yourmajesty.co>
2024-06-16 21:03:13 +0300

    NTA-001, added config files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        Save changes
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -85,9 +83,7 @@ export default function Home() {
           <h2>
             Deploy <span>-&gt;</span>
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
         </a>
       </div>
     </main>
